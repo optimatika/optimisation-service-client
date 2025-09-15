5f0d45ade3d12fdb3340b14282679724dff3fc18
apete <apete@optimatika.se>
2025-09-15 07:52:04 +0200

    test

--- src/test/java/se/optimatika/optimisation/service/client/OptimisationServiceClientTest.java
+++ src/test/java/se/optimatika/optimisation/service/client/OptimisationServiceClientTest.java
@@ -16,13 +16,13 @@ import org.ojalgo.optimisation.ExpressionsBasedModel;
 
 public class OptimisationServiceClientTest {
 
-    // private static final String HOST = "http://optimatika-service-296829693201.europe-north1.run.app";
+    //private static final String HOST = "http://optimatika-service-296829693201.europe-north1.run.app";
     // private static final String HOST = "http://16.16.99.66:8080";
-    private static final String HOST = "http://localhost:8080";
-    //private static final String HOST = "https://optimatika-boot-services-969062758986.europe-north1.run.app";
+    //private static final String HOST = "http://localhost:8080";
+    private static final String HOST = "https://optimatika-boot-services-969062758986.europe-north1.run.app";
     //private static final String HOST = "https://test-service.optimatika.se";
 
-    static final boolean DEBUG = false;
+    static final boolean DEBUG = true;
 
     @AfterEach
     public void clearIntegrations() {
